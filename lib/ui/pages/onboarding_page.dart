27346fbec3e120abf4c8a686e9220680f2e0c8d2
dandip4 <nugrahadanadipa@gmail.com>
2024-10-08 01:15:51 +0700

    hari 1

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -1,4 +1,6 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/pages/sign_in_page.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -101,27 +103,9 @@ class _OnboardingPageState extends State<OnboardingPage> {
                   currentIndex == 2 ?
                   Column(
                     children: [
-                       SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: TextButton(
-                          onPressed: () {
-                            carouselController.nextPage();
-                          },
-                          style: TextButton.styleFrom(
-                            backgroundColor: purpleColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(56),
-                            ),
-                          ),
-                          child: Text(
-                            'Get Started',
-                            style: whiteTextStyle.copyWith(
-                              fontSize: 16,
-                              fontWeight: semiBold,
-                            ),
-                          ),
-                        ),
+                      CustomFilledButton(
+                        title : 'Get Started',
+                        onPressed: () {},
                       ),
                       const SizedBox(
                         height: 20,
@@ -131,12 +115,13 @@ class _OnboardingPageState extends State<OnboardingPage> {
                         height: 24,
                         child: TextButton(
                           onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => const SignInPage()));
                           },
                           style: TextButton.styleFrom(
                              padding: EdgeInsets.zero,
                           ),
                           child: Text(
-                            'Sign IN',
+                            'Sign In',
                             style: greyTextStyle.copyWith(
                               fontSize: 16,
                             ),
@@ -187,27 +172,13 @@ class _OnboardingPageState extends State<OnboardingPage> {
                         ),
                       ),
                       const Spacer(),
-                      SizedBox(
+                      CustomFilledButton(
                         width: 150,
-                        height: 50,
-                        child: TextButton(
-                          onPressed: () {
-                            carouselController.nextPage();
-                          },
-                          style: TextButton.styleFrom(
-                            backgroundColor: purpleColor,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(56),
-                            ),
-                          ),
-                          child: Text(
-                            'Continue',
-                            style: whiteTextStyle.copyWith(
-                              fontSize: 16,
-                              fontWeight: semiBold,
-                            ),
-                          ),
-                        ),
+                        title: 'Continue',
+                        onPressed: () {
+                          carouselController.nextPage();
+
+                        }
                       ),
                     ],
                   ),
